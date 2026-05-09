f35acb48f1c57a0e175b57d2be3a81f7b743c57c
conceicaodomingos <conceicaojpdomingos@gmail.com>
2026-05-09 13:31:06 +0100

    Criando a funcionalidade de cadastro de usuario

--- src/main/java/br/com/treinaweb/ediaristas/web/controllers/UsuarioController.java
+++ src/main/java/br/com/treinaweb/ediaristas/web/controllers/UsuarioController.java
@@ -2,12 +2,18 @@ package br.com.treinaweb.ediaristas.web.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.com.treinaweb.ediaristas.web.dtos.FlashMessages;
 import br.com.treinaweb.ediaristas.web.dtos.UsuarioCadastroForm;
 import br.com.treinaweb.ediaristas.web.services.WebUsuarioService;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/admin/usuarios")
@@ -29,4 +35,17 @@ public class UsuarioController {
     modelAndView.addObject("cadastroForm", new UsuarioCadastroForm());
     return modelAndView;
   }
+
+  @PostMapping("/cadastrar")
+  public String cadastrar(@Valid @ModelAttribute("cadastroForm") UsuarioCadastroForm cadastroForm, BindingResult result, RedirectAttributes attrs) {
+
+    if (result.hasErrors()) {
+      return "admin/usuario/cadastro-form";
+    }
+
+    service.cadastrar(cadastroForm);
+    attrs.addFlashAttribute("alert", new FlashMessages("alert-success", "Usuário cadastrado com sucesso!"));
+    
+    return "redirect:/admin/usuarios";
+  }  
 }
