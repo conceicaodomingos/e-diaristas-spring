151b899bd8e601e944038d693909c6e9de70a7ab
conceicaodomingos <conceicaojpdomingos@gmail.com>
2026-05-10 17:18:12 +0100

    Criando a pagina de edicao de usuario

--- src/main/java/br/com/treinaweb/ediaristas/web/services/WebUsuarioService.java
+++ src/main/java/br/com/treinaweb/ediaristas/web/services/WebUsuarioService.java
@@ -10,6 +10,7 @@ import br.com.treinaweb.ediaristas.core.exceptions.UsuarioNaoEncontradoException
 import br.com.treinaweb.ediaristas.core.models.Usuario;
 import br.com.treinaweb.ediaristas.core.repositories.UsuarioRepository;
 import br.com.treinaweb.ediaristas.web.dtos.UsuarioCadastroForm;
+import br.com.treinaweb.ediaristas.web.dtos.UsuarioEdicaoForm;
 import br.com.treinaweb.ediaristas.web.mappers.WebUsuarioMapper;
 
 @Service
@@ -34,6 +35,11 @@ public class WebUsuarioService {
   public Usuario buscarPorId(Long id) {
     var mensagem = String.format("Usuário com ID %d não encontrado.", id);
     return repository.findById(id).orElseThrow(() -> new UsuarioNaoEncontradoException(mensagem));
+  }
+
+  public UsuarioEdicaoForm buscarFormPorId(Long id) {
+    var usuario = buscarPorId(id);
+    return mapper.toForm(usuario);
   }
 
   public void excluirPorId(Long id) {
