d2cf97755dd56a53ff646e0b64247e80530a6f48
conceicaodomingos <conceicaojpdomingos@gmail.com>
2026-05-11 06:29:36 +0100

    Criando a validacao de confirmacao de senha

--- src/main/java/br/com/treinaweb/ediaristas/web/services/WebUsuarioService.java
+++ src/main/java/br/com/treinaweb/ediaristas/web/services/WebUsuarioService.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.FieldError;
 
 import br.com.treinaweb.ediaristas.core.enums.TipoUsuario;
+import br.com.treinaweb.ediaristas.core.exceptions.SenhasNaoConferemException;
 import br.com.treinaweb.ediaristas.core.exceptions.UsuarioNaoEncontradoException;
 import br.com.treinaweb.ediaristas.core.models.Usuario;
 import br.com.treinaweb.ediaristas.core.repositories.UsuarioRepository;
@@ -27,8 +29,19 @@ public class WebUsuarioService {
   }
 
   public Usuario cadastrar(UsuarioCadastroForm form) {
+    var senha = form.getSenha();
+    var confirmacaoSenha = form.getConfirmacaoSenha();
+
+    if (!senha.equals(confirmacaoSenha)) {
+      var mensagem = "Os campos de senha não conferem";
+      var fieldError = new FieldError(form.getClass().getName(), "confirmacaoSenha", form.getConfirmacaoSenha(), false, null, null, mensagem);
+
+      throw new SenhasNaoConferemException(mensagem, fieldError);
+    }
+
     var model = mapper.toModel(form);
     model.setTipoUsuario(TipoUsuario.ADMIN);
+    
     return repository.save(model);
   }
 
